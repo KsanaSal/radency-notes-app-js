63e6f4ff07bd83820e7c890c2067f63b9c3a253e
KsanaSal <oasalivon@gmail.com>
2023-07-28 19:41:06 +0300

    + added init data

---
+++ src/js/mockData/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        categoryId: "1",
+        categoryName: "Task",
+        categoryImg: require(`../../images/icon-list-check.svg`),
+    },
+    {
+        categoryId: "2",
+        categoryName: "Random Thought",
+        categoryImg: require(`../../images/icon-random-thought.svg`),
+    },
+    {
+        categoryId: "3",
+        categoryName: "Idea",
+        categoryImg: require(`../../images/icon-idea.svg`),
+    },
+    {
+        categoryId: "4",
+        categoryName: "Quote",
+        categoryImg: require(`../../images/icon-quote.svg`),
+    },
+];
+
+export default categories;
