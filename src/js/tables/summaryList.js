47b332e6857af28c0358d532ba5c2aaf868d4ac7
KsanaSal <oasalivon@gmail.com>
2023-07-29 14:02:37 +0300

    + added rendered the summary table

--- src/js/tables/summaryList.js
+++ src/js/tables/summaryList.js
@@ -1,3 +1,49 @@
+import initData from "../mockData/initData";
+import categories from "../mockData/categories";
+import emitter from "../eventEmitter";
+
+const countNotesCategory = (categoryId, archived) =>
+    initData.filter(
+        (item) => item.categoryId === categoryId && item.archived === archived
+    ).length;
+
+emitter.on("dataChanged", () => {
+    const tableBody = summaryList.querySelector("tbody");
+    tableBody.innerHTML = generateTableRows();
+});
+
+const generateTableRows = () => {
+    let rowsHtml = "";
+
+    for (const category of categories) {
+        const { categoryId, categoryName, categoryImg } = category;
+        const activeCount = countNotesCategory(categoryId, false);
+        const archivedCount = countNotesCategory(categoryId, true);
+
+        const rowHtml = `
+                <tr
+                    class="flex items-center text-left  py-2 px-4 bg-white border border-teal-500 rounded-md overflow-hidden shadow-sm hover:shadow-md"
+                >
+                <td class="w-[70px] shrink-0 overflow-ellipsis whitespace-nowrap overflow-hidden">
+                <div class="bg-teal-300 p-2 rounded-full flex justify-center w-[50px] h-[50px]">
+                    <img src="${category.categoryImg}" alt="${category.categoryName}" width="20" height="20"/>
+                </div>
+                    <td class="w-[550px] shrink-0 text-gray-800 font-semibold">${categoryName}</td>
+                    <td class="w-[270px] shrink-0">
+                        ${activeCount}
+                    </td>
+                    <td class="w-[270px] shrink-0">
+                        ${archivedCount}
+                    </td>
+                </tr>
+    `;
+
+        rowsHtml += rowHtml;
+    }
+
+    return rowsHtml;
+};
+
 const summaryList = document.createElement("section");
 summaryList.classList.add("text-gray-800", "mt-[30px]");
 const tableSummaryList = /*html*/ `
@@ -16,31 +62,16 @@ const tableSummaryList = /*html*/ `
         >
             <thead>
                 <tr
-                    class=" flex justify-between text-left text-[18px] font-semibold py-2 px-4 bg-teal-200 border border-teal-700 rounded-[4px] overflow-hidden shadow-sm hover:shadow-md"
+                    class=" flex text-left text-[18px] font-semibold py-2 px-4 bg-teal-200 border border-teal-700 rounded-[4px] overflow-hidden shadow-sm hover:shadow-md"
                 >
-                    <th class="w-[60px] shrink-0">Images</th>
-                    <th class="w-[130px] shrink-0">Note Category</th>
-                    <th class="w-[150px] shrink-0">Active</th>
-                    <th class="w-[100px] shrink-0">Archived</th>
+                    <th class="w-[70px] shrink-0"></th>
+                    <th class="w-[550px] shrink-0">Note Category</th>
+                    <th class="w-[270px] shrink-0">Active</th>
+                    <th class="w-[270px] shrink-0">Archived</th>
                 </tr>
             </thead>
-            <tbody class=" flex flex-col gap-[10px] text-gray-600">
-                <tr
-                    class="flex justify-between items-center text-left  py-2 px-4 bg-white border border-teal-500 rounded-md overflow-hidden shadow-sm hover:shadow-md"
-                >
-                    <td
-                        class="w-[60px] shrink-0 overflow-ellipsis whitespace-nowrap overflow-hidden"
-                    >
-                        images
-                    </td>
-                    <td class="w-[130px] shrink-0 text-gray-800 font-semibold">Random Thought</td>
-                    <td class="w-[150px] shrink-0">
-                        2
-                    </td>
-                    <td class="w-[100px] shrink-0">
-                        1
-                    </td>
-                </tr>
+            <tbody class="flex flex-col gap-[10px] text-gray-600">
+                ${generateTableRows()}
             </tbody>
         </table>`;
 summaryList.innerHTML = tableSummaryList;
